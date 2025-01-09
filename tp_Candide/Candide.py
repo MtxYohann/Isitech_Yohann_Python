7ecc8630e3064afd9586cfc00bac4cadd2f21755
MtxYohann <137070655+Yohann-mtx@users.noreply.github.com>
2025-01-09 12:24:46 +0100

    Candide print plus lisible

--- tp_Candide/Candide.py
+++ tp_Candide/Candide.py
@@ -38,9 +38,8 @@ with open(output_file_path, 'w', encoding='utf-8') as f:
     for _, caracteres in caracteres_groupes_punct:
         f.write(caracteres + '\n')
 
-
 # partie statistiques
-
+print("\n\n-------------------")
 # Compter les mots
 mots = re.findall(r'\b\w+\b', texte)
 nombre_de_mots = len(mots)
@@ -57,6 +56,9 @@ print("Statistiques du texte :")
 print(f"Nombre de mots : {nombre_de_mots}")
 print(f"Nombre de phrases : {nombre_de_phrases}")
 print(f"Nombre de voyelles : {nombre_de_voyelles}")
+print("-------------------")
+
+print("\n\n-------------------")
 
 # Partie Statistiques plus précises
 print("Statistiques plus précises :")
@@ -74,6 +76,8 @@ mots_count = mots_count[:5]
 print("Mots 5 mots les plus utilisés :")
 for mot, count in mots_count:
     print(f"{mot} : {count}")
+print("-------------------")
+print("\n -------------------")
 
 # les mots avec plus de 7 lettres
 mots = re.findall(r'\b\w+\b', texte)
@@ -82,10 +86,10 @@ mots_count = defaultdict(int)
 for mot in mots:
     if len(mot) > 7:
         mots_count[mot] += 1
-        print(mot)
 mots_count = sorted(mots_count.items(), key=lambda x: x[1], reverse=True)
 mots_count = [(mot, count) for mot, count in mots_count if count > 1]
 mots_count = mots_count[:5]
 print("Mots avec plus de 7 lettres :")
 for mot, count in mots_count:
     print(f"{mot} : {count}")
+print("-------------------")
